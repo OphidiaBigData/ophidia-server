ac3b6002af81d9a2d202a67ee338e74226d555dd
Cosimo Palazzo <cosimo.palazzo@cmcc.it>
2017-08-02 12:34:31 +0200

    Drop the reference to email in authorization request for OpenId Connect

--- etc/web/openid.php
+++ etc/web/openid.php
@@ -135,7 +135,7 @@ xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/">
 			} else {
 				$continue = true;
 				$nonce = openssl_random_pseudo_bytes(10);
-				header('Location: '.$oph_openid_endpoint.'/authorize?response_type=code&client_id='.$oph_openid_client_id.'&scope=openid+profile+email+offline_access&redirect_uri='.$oph_web_server_secure.'/openid.php&nonce=',$nonce);
+				header('Location: '.$oph_openid_endpoint.'/authorize?response_type=code&client_id='.$oph_openid_client_id.'&scope=openid+profile+offline_access&redirect_uri='.$oph_web_server_secure.'/openid.php&nonce=',$nonce);
 			}
 		}
 	}
